cd02f6476af2d4a4e38b45462c83b0767a62b63e
kawashiii <turquoise12-1@hotmail.co.jp>
2019-12-02 17:28:21 +0900

    fix data_overview.py for checking source image * fix make_json for including source label

--- lab/data_overview.py
+++ lab/data_overview.py
@@ -3,8 +3,31 @@ import sys
 import json
 
 ## Overview train data
-print("================  Training Data Overview  ===============")
-print()
+print("================  Training Data Overview (Source) ===============")
+print("")
+class_names = {}
+image_count = 0
+for curDir, dirs, files in os.walk("./train"):
+    if os.path.isfile(curDir + "/" + "label.json"):
+        data = json.load(open(curDir + "/" + "label.json"))
+        data = list(data.values())
+        for d in data:
+            regions = d["regions"]
+            if regions != []:
+                image_count += 1
+                for r in regions:
+                    name = r["region_attributes"]["name"]
+                    if class_names.get(name): class_names[name] += 1
+                    else: class_names[name] = 1
+
+print("The number of Train Image: ", image_count)
+print("The number of Train Label: ", sum(class_names.values()))
+for k, v in sorted(class_names.items()):
+    print("    '" + k + "' label: ", v)
+
+print("")
+print("================  Training Data Overview (Source + Augmented) ===============")
+print("")
 target_file = "./train/train_data.json"
 
 data = json.load(open(target_file))
@@ -26,11 +49,34 @@ print("The number of Train Label: ", sum(class_names.values()))
 for k, v in sorted(class_names.items()):
     print("    '" + k + "' label: ", v)
 
-print()
+print("")
 
 ## Overview validation data
-print("================  Validation Data Overview  ===============")
-print()
+print("================  Validation Data Overview (Source) ===============")
+print("")
+class_names = {}
+image_count = 0
+for curDir, dirs, files in os.walk("./val"):
+    if os.path.isfile(curDir + "/" + "label.json"):
+        data = json.load(open(curDir + "/" + "label.json"))
+        data = list(data.values())
+        for d in data:
+            regions = d["regions"]
+            if regions != []:
+                image_count += 1
+                for r in regions:
+                    name = r["region_attributes"]["name"]
+                    if class_names.get(name): class_names[name] += 1
+                    else: class_names[name] = 1
+
+print("The number of Validation Image: ", image_count)
+print("The number of Validation Label: ", sum(class_names.values()))
+for k, v in sorted(class_names.items()):
+    print("    '" + k + "' label: ", v)
+
+print("")
+print("================  Validation Data Overview (Total)  ===============")
+print("")
 target_file = "./val/val_data.json"
 
 data = json.load(open(target_file))
--- lab/make_json.py
+++ lab/make_json.py
@@ -18,7 +18,16 @@ CURRENT_DIR += args[1] + "/"
 result = {}
 
 for curDir, dirs, files in os.walk(CURRENT_DIR):
-    if curDir.endswith("augmented") and os.path.isfile(curDir + "/" + "label_augment.json"):
+    if os.path.isfile(curDir + "/" + "label.json"):
+        f = open(curDir + "/" + "label.json")
+        data = json.load(f)
+        for x in data:
+            filename = curDir + "/" + data[x]["filename"]
+            data[x]["filename"] = filename[len(args[1]) + 1 + 2:]
+            #print(filename)
+        result.update(data)
+    
+    if os.path.isfile(curDir + "/" + "label_augment.json"):
         f = open(curDir + "/" + "label_augment.json")
         data = json.load(f)
         for x in data:
